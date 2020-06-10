1b018c80944ae8f71a7c6cd985b5e5daa31ed87e
theluispestana <theluispestana@gmail.com>
2020-06-10 12:44:44 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,40 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render() {
+    const { name, sprites, stats } = this.props.pokemon;
+    const stat = stats.filter((obj) => obj.name === "hp");
     return (
       <Card>
-        <div>
+        <div onClick={this.handleClick}>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              src={this.state.clicked ? sprites.back : sprites.front}
+              alt="oh no!"
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {stat[0].value + " hp"}
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,34 +1,64 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from "react";
+import { Form } from "semantic-ui-react";
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: "",
+    };
   }
 
   render() {
+    const { name, front, back, hp } = this.props.newPokemon;
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
-          <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+        <Form
+          onSubmit={() => {
+            console.log("submitting form...");
+          }}
+        >
+          <Form.Group widths="equal" onChange={this.props.handleForm}>
+            <Form.Input
+              fluid
+              label="Name"
+              placeholder="Name"
+              name="name"
+              value={name}
+            />
+            <Form.Input
+              fluid
+              label="hp"
+              placeholder="hp"
+              name="hp"
+              type="number"
+              value={hp}
+            />
+            <Form.Input
+              fluid
+              label="Front Image URL"
+              placeholder="url"
+              name="front"
+              value={front}
+            />
+            <Form.Input
+              fluid
+              label="Back Image URL"
+              placeholder="url"
+              name="back"
+              value={back}
+            />
           </Form.Group>
-          <Form.Button>Submit</Form.Button>
+          <Form.Button onClick={this.props.handleSubmit}>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
