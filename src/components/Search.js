2974fd9fe9cce103bdb2328ceba477b67589ef6f
github username <github email address>
2020-06-10 21:24:44 -0400

    Done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Search = props => {
-  return (
+
+  return (    
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" onChange={() => {console.log("searching...")}} />
+        <input className="prompt" onChange={props.handleSearch} />
         <i className="search icon" />
       </div>
     </div>
