67fb7467cc62224decf245c72961187b1ff01122
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-10 23:50:40 -0700

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Form } from 'semantic-ui-react'
 
+const POKE_URL = "http://localhost:3000/pokemon"
+
 class PokemonForm extends React.Component {
   constructor() {
     super()
@@ -13,16 +15,71 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(POKE_URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        stats: [{value: this.state.hp, name: "hp"}],
+        sprites: {
+          front: this.state.frontUrl,
+          back: this.state.backUrl
+        }
+      })
+    })
+      .then(resp => resp.json())
+      .then(pokemon => {
+        this.props.addPokemon(pokemon)
+      })
+  };
+
   render() {
+    console.log(this.state)
+    const {name, hp, frontUrl, backUrl} = this.state
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input 
+              fluid label="Name" 
+              placeholder="Name" 
+              name="name" 
+              value={name} 
+              onChange={this.handleChange}
+            />
+            <Form.Input 
+              fluid label="hp" 
+              placeholder="hp" 
+              name="hp" 
+              value={hp} 
+              onChange={this.handleChange}
+            />
+            <Form.Input 
+              fluid label="Front Image URL" 
+              placeholder="url"   
+              name="frontUrl" 
+              value={frontUrl} 
+              onChange={this.handleChange}
+            />
+            <Form.Input 
+              fluid label="Back Image URL" 
+              placeholder="url" 
+              name="backUrl" 
+              value={backUrl} 
+              onChange={this.handleChange}
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,17 +4,51 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+const POKE_URL = "http://localhost:3000/pokemon"
+
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    search: '',
+  }
+
+  componentDidMount(){
+    fetch(POKE_URL)
+      .then(resp => resp.json())
+      .then(pokemons => this.setState({ pokemons }))
+  }
+
+  handleSearch = (e) => {
+    this.setState({ search: e.target.value})
+  };
+
+  addPokemon = (pokemon) => {
+    let newArray = [...this.state.pokemons]
+    newArray.unshift(pokemon)
+    this.setState({ pokemons: newArray })
+  };
+
   render() {
+
+    const {pokemons, search} = this.state
+    
+    const filteredPokemons = pokemons.filter(
+      pokemon => pokemon.name.includes(search.toLowerCase()))
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon}/>
         <br />
-        <Search />
+        <Search 
+          search={this.state.search} 
+          handleSearch={this.handleSearch}
+          filterPokemons={this.filterPokemons}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={filteredPokemons}/>
       </Container>
     )
   }
